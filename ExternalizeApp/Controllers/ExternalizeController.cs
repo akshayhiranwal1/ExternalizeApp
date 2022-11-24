20cec0e09b6c8d4d4e9b29d4df81416df659ecdf
akshayhiranwal1 <akshayhiranwal1@gmail.com>
2022-11-24 14:51:26 +0000

    Initial check-in of module ExternalizeApp

---
+++ ExternalizeApp/Controllers/ExternalizeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ExternalizeApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExternalizeController: ControllerBase
+    {
+        public IConfiguration Configuration { get; }
+        public ExternalizeController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        [HttpGet]
+        public Dictionary<string,string> Get()
+        {
+            return Configuration
+                .GetSection("Configurations")
+                .GetChildren()
+                .ToDictionary(a => a.Key, a => a.Value);
+        }
+    }
+}
---
+++ ExternalizeApp/Startup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore;
+
+namespace ExternalizeApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(i =>
+            {
+                i.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo()
+                {
+                    Title = "MyApp",
+                    Version="v1"
+                }); ;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(i =>
+            {
+                i.SwaggerEndpoint("/swagger/v1/swagger.json", "MyApp API");
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
